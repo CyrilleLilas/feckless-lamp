91af4349a39b5ba3a8630b993a395f70036074c3
Cyrille <pancakos@gmail.com>
2015-10-14 18:49:49 +0200

    app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FecklessLamp::Application.config.secret_key_base = '57f951149e16df4687ff9f5bee93e9cdf296456456471080b0f9cb2270e2b625763a0a2c2b3d4e90f1453bd50e9ca76c89dea76f2223fb390510f70c43829ced'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FecklessLamp::Application.config.session_store :cookie_store, key: '_feckless-lamp_session'
