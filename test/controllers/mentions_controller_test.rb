86c4ff0f56d329f7e5494dbc941e2e82a7dfaa07
Cyrille <pancakos@gmail.com>
2015-10-20 19:52:31 +0200

    controller tests

--- test/controllers/mentions_controller_test.rb
+++ test/controllers/mentions_controller_test.rb
@@ -2,48 +2,26 @@ require 'test_helper'
 
 class MentionsControllerTest < ActionController::TestCase
   setup do
-    @mention = mentions(:one)
+    mentions(:one)
   end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:mentions)
+    assert_template :index
+    assert_template layout: "layouts/application"
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
+  test "should fetch new mentions" do
+    post :fetch, :format => :json, since_id: 1
+    assert_not_nil assigns(:new_mentions)
+    assert_template :fetch
   end
 
-  test "should create mention" do
-    assert_difference('Mention.count') do
-      post :create, mention: { body: @mention.body }
-    end
-
-    assert_redirected_to mention_path(assigns(:mention))
+  test "should reply to a tweet" do
+    post :reply, in_reply_to_status_id: 1, reply: "hello"
+    assert_redirected_to '/'
   end
 
-  test "should show mention" do
-    get :show, id: @mention
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @mention
-    assert_response :success
-  end
-
-  test "should update mention" do
-    patch :update, id: @mention, mention: { body: @mention.body }
-    assert_redirected_to mention_path(assigns(:mention))
-  end
-
-  test "should destroy mention" do
-    assert_difference('Mention.count', -1) do
-      delete :destroy, id: @mention
-    end
-
-    assert_redirected_to mentions_path
-  end
 end
